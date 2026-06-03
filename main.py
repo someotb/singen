55bb8077fae5422ec348602c5e9ae9eadaf9a791
someotb <llyryk934@gmail.com>
2026-06-03 21:14:39 +0700

    feat: add signal spectre

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+FS = 1000
+UP_KOEF = 3
+
 
 def sin_gen(freq: int, duration: int):
-    fs = 1001
-    fs_step = 1 / fs
-    fs_list = np.arange(0, duration, fs_step)
-    sin = np.sin(2 * np.pi * freq * fs_list)
+    fs_step = 1 / FS
+    fs_list = np.arange(0, duration - fs_step, fs_step)
+    sin = np.sin(2 * np.pi * freq * fs_list + 1)
     return sin, fs_list
 
 
@@ -20,13 +22,25 @@ def interpolation(signal, up_koef: int):
     return signal_up
 
 
-sin, t = sin_gen(500, 1)
-sin_up = interpolation(sin, 3)
+sin, t = sin_gen(40, 1)
+sin_up = interpolation(sin, UP_KOEF)
+
+fft_sin = np.abs(np.fft.fft(sin))
+fft_freq = np.fft.fftfreq(len(sin), 1 / FS)
+
+fft_sin_up = np.abs(np.fft.fft(sin_up))
+fft_freq_up = np.fft.fftfreq(len(sin_up), 1 / (FS * UP_KOEF))
 
 plt.figure()
 plt.plot(t, sin)
 
 plt.figure()
 plt.plot(sin_up)
+
+plt.figure()
+plt.subplot(2, 1, 1)
+plt.plot(fft_freq, fft_sin)
+plt.subplot(2, 1, 2)
+plt.plot(fft_freq_up, fft_sin_up)
 
 plt.show()
